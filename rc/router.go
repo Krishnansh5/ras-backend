bab7cf15dac168196a0ce3643dd8fd68148c27ab
Krishnansh5 <5272.krishnansh@gmail.com>
2024-02-10 01:39:50 +0530

    sync button changes from main to dev

--- rc/router.go
+++ rc/router.go
@@ -17,7 +17,7 @@ func AdminRouter(mail_channel chan mail.Mail, r *gin.Engine) {
 		admin.GET("/count", getRCCountHandler)
 
 		admin.GET("/notice", getAllNoticesHandler)
-		admin.POST("/notice", postNoticeHandler)
+		admin.POST("/notice", postNoticeHandler(mail_channel))
 		admin.PUT("/notice", putNoticeHandler)
 		admin.POST("/notice/:nid/reminder", postReminderHandler(mail_channel))
 		admin.DELETE("/notice/:nid", deleteNoticeHandler)
@@ -38,6 +38,10 @@ func AdminRouter(mail_channel chan mail.Mail, r *gin.Engine) {
 		admin.PUT("/student", putStudentHandler)
 
 		admin.PUT("/student/freeze", bulkFreezeStudentsHandler(mail_channel))
+
+		admin.PUT("/student/sync", syncStudentsHandler)
+		// route not in use
+		// admin.PUT("/student/deregister", deregisterAllStudentsHandler)
 
 		admin.GET("/student/questions", getStudentQuestionsHandler)
 		admin.POST("/student/question", postStudentQuestionHandler)
